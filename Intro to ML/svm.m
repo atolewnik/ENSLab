bb7a5da9a9cc8fae84aac2f84686fcb5af1e9fc1
lagadhmatsa <44494160+lagadhmatsa@users.noreply.github.com>
2023-01-29 18:09:39 -0500

    Add code for svm mnist

--- Intro to ML/svm.m
+++ Intro to ML/svm.m
@@ -1,1 +1,18 @@
+clc
+clear all
+close all
 
+images = loadMNISTImages('../MNIST_Dataset/train-images.idx3-ubyte');
+labels = loadMNISTLabels('../MNIST_Dataset/train-labels.idx1-ubyte');
+
+test_images = loadMNISTImages('../MNIST_Dataset/t10k-images.idx3-ubyte');
+test_labels = loadMNISTLabels('../MNIST_Dataset/t10k-labels.idx1-ubyte');
+
+images_T = transpose(images);
+test_images_T = transpose(test_images);
+
+dataset = [images_T labels];
+
+Mdl = fitcecoc(images_T,labels);
+
+test_predict = predict(Mdl, test_images_T);
