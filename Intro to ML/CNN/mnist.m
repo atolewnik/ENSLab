7e369d76d1430caad6aece6b52a557b3762f4a17
lagadhmatsa <44494160+lagadhmatsa@users.noreply.github.com>
2023-01-29 18:26:45 -0500

    Add neural network related MATLAB code

--- Intro to ML/CNN/mnist.m
+++ Intro to ML/CNN/mnist.m
@@ -1,1 +1,64 @@
+% https://www.mathworks.com/help/deeplearning/ug/create-simple-deep-learning-network-for-classification.html
+% https://www.mathworks.com/help/deeplearning/ug/train-a-variational-autoencoder-vae-to-generate-images.html
+clc
+clear all
+close all
 
+digitDatasetPath = fullfile(matlabroot,'toolbox','nnet','nndemos', 'nndatasets','DigitDataset');
+imds = imageDatastore(digitDatasetPath, 'IncludeSubfolders',true,'LabelSource','foldernames');
+
+figure;
+perm = randperm(10000,20);
+for i = 1:20
+    subplot(4,5,i);
+    imshow(imds.Files{perm(i)});
+end
+
+labelCount = countEachLabel(imds);
+
+img = readimage(imds,1);
+size(img);
+
+numTrainFiles = 750;
+[imdsTrain,imdsValidation] = splitEachLabel(imds,numTrainFiles,'randomize');
+
+layers = [
+    imageInputLayer([28 28 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer
+  ];
+
+options = trainingOptions('sgdm', 'InitialLearnRate', 0.01, 'MaxEpochs', 4, ...
+                'Shuffle','every-epoch', ...
+            'ValidationData',imdsValidation, 'ValidationFrequency', ...
+            30, 'Verbose', false, 'Plots','training-progress');
+
+net = trainNetwork(imdsTrain,layers,options);
+
+YPred = classify(net,imdsValidation);
+YValidation = imdsValidation.Labels;
+
+accuracy = sum(YPred == YValidation)/numel(YValidation);
+
+% 
+% img = readimage(imds,1);
+% imshow(img);
+% YPred = classify(net,img);
